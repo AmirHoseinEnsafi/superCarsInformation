ba4c0b66d0489cd7ecde79b2bc7903550ad789b8
AmirHosineEnsafi <amirmahdion1@gmail.com>
2024-03-28 14:38:22 +0330

    add feacher of the post in the core of the code but it need to check the schema becouse its need to portect from the additional key from the post man

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express =  require('express');
 const app = express();
-
+app.use(express.json())
 const Joi = require('joi')
-
+const schema = require('./validation.js')
 const supercar = require('./class.js');
 
 const SuperSportCar = supercar;
@@ -32,3 +32,16 @@ app.get("/api/informations/:id" , (req , res) =>{
 })
 
 app.listen(3000 , () => console.log("listening on port 3000"))
+
+app.post("/api/informations" , (req , res) =>{
+    const {error} = schema.validate(req.body);
+    if(error)res.status(400).send(error.details[0].message);
+    else {
+        const car = new Object()
+        car.id = SuperSportCar.length +1;
+        const c = schema.validate(req.body)
+        Object.assign(car , c.value)
+        SuperSportCar.push(car);
+        res.send(car)
+    }
+})
---
+++ validation.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+const schema = Joi.object({
+    CompanyName : Joi.string().min(3).required(),
+    CarName : Joi.string().min(3).required(),
+    Engine : Joi.string().min(3),
+    GearBox : Joi.string(),
+    PerformanceCar : Joi.string(),
+    DriveWheel : Joi.string(),
+    TopSpeed : Joi.string().min(3).required(),
+    Price : Joi.string().min(5).required(),
+    Acceleration : Joi.string(),
+    door : Joi.number()
+})
+module.exports = schema
